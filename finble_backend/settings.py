51ddb1ebbf06239471b8c86f3775882f9db3e3e6
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2022-12-29 16:29:44 +0900

    Update settings.py

--- finble_backend/settings.py
+++ finble_backend/settings.py
@@ -17,7 +17,7 @@ import pymysql
 pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 env = environ.Env(
     DEBUG=(bool, False)
 )
