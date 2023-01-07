f353ac1b105ac53a91afcb9e75bf20fb5a26eeb2
geniee44 <68368633+geniee44@users.noreply.github.com>
2023-01-08 02:03:54 +0900

    fix nan error

--- stock.py
+++ stock.py
@@ -1,14 +1,14 @@
+import os
+import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finble_backend.settings')
+django.setup()
+
 import FinanceDataReader as fdr
 import datetime
 from datetime import datetime, date
 from dateutil.relativedelta import relativedelta
-from models import *
-
-import os
-import django
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BASE_DIR.finble_backend.settings')
-django.setup()
+from finble.models import *
+import numpy as np
 
 
 def get_price_data():
@@ -16,9 +16,9 @@ def get_price_data():
     now = datetime.now()
     before = now - relativedelta(years=10)
 
-    check = 0
     for stock in stock_list:
-        datas = fdr.DataReader(stock.symbol, before, now)['Close']
+        # datas = fdr.DataReader(stock.symbol, before, now)['Close']
+        datas = fdr.DataReader(stock.symbol, '2012')['Close']
         date = datas.index
 
         for i in date:
@@ -31,13 +31,14 @@ def get_price_data():
 
         print(f'Symbol:{stock.symbol:8} Market:{stock.market:6} Name:{stock.name:8} Count:{len(datas):4}')
 
-    print(f'FINISH update Price table: running time {datetime.now() - now} secs')
+    print(f'FINISH update Price table: running time {datetime.now() - now} secs & {len(datas)} datas')
 
 
 def get_kospi_data():
     now = datetime.now()
     before = now - relativedelta(years=1)
-    datas = fdr.DataReader('KS11', before, now)['Close']
+    # datas = fdr.DataReader('KS11', before, now)['Close']
+    datas = fdr.DataReader('KS11', '2022')['Close']
     date = datas.index
 
     for i in date:
@@ -52,20 +53,24 @@ def get_kospi_data():
 def get_exchangerate_data():
     now = datetime.now()
     before = now - relativedelta(years=10)
-    datas = fdr.DataReader('USD/KRW', before, now)['Close']
+    # datas = fdr.DataReader('USD/KRW', before, now)['Close']
+    datas = fdr.DataReader('USD/KRW', '2012')['Close']
+    datas = datas.replace({np.nan: None})
     date = datas.index
 
-    print(datas)
-
     for i in date:
-        ExchangeRate.objects.create(
-            date=i.date(),
-            rate=datas[i]
-        )
+        try:
+            ExchangeRate.objects.create(
+                date=i.date(),
+                rate=datas[i]
+            )
+        except:
+            print(i.date())
+            continue
 
     print(f'FINISH update Exchange Rate table: running time {datetime.now() - now} secs & {len(datas)} datas')
 
 
-get_price_data()
-get_kospi_data()
+# get_price_data()
+# get_kospi_data()
 get_exchangerate_data()
