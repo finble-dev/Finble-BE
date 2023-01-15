87e23212e9c8fa0c1e8fb5f25e1c76a6ec41ec6f
geniee44 <68368633+geniee44@users.noreply.github.com>
2023-01-15 22:09:48 +0900

    upgrade stock.py by using asyincio

--- stock.py
+++ stock.py
@@ -8,20 +8,27 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from finble.models import *
 import numpy as np
+import asyncio
+from asgiref.sync import sync_to_async
 
 
-def get_price_data():
+@sync_to_async
+def get_price_data(num):
     stock_list = Stock.objects.all()
     now = datetime.now()
-    before = now - relativedelta(years=10)
+    start = now.year - num
+    end = str(start + 1)
 
     for stock in stock_list:
-        # datas = fdr.DataReader(stock.symbol, before, now)['Close']
-        datas = fdr.DataReader(stock.symbol, '2012', '2022-12-31')['Close']
+        try:
+            datas = fdr.DataReader(stock.symbol, str(start), end)['Close']
+        except:
+            print(f'Got Error on Symbol:{stock.symbol:8} Date:{start} - {end}')
+            continue
+
         date = datas.index
 
         for i in date:
-            # print(i, "|", datas[i])
             Price.objects.create(
                 symbol=Stock.objects.get(symbol=stock.symbol),
                 date=i.date(),
@@ -30,14 +37,21 @@ def get_price_data():
 
         print(f'Symbol:{stock.symbol:8} Market:{stock.market:6} Name:{stock.name:8} Count:{len(datas):4}')
 
-    print(f'FINISH update Price table: running time {datetime.now() - now} secs & {len(datas)} datas')
+    print(f'======FINISH update Price table: running time {datetime.now() - now} secs======')
+
+
+async def update_price_data_to_db():
+    today = datetime.now().year
+
+    coroutines = [get_price_data(i) for i in range(11, 0, -1)]
+    await asyncio.gather(*coroutines)
 
 
 def get_kospi_data():
     now = datetime.now()
     before = now - relativedelta(years=1)
     # datas = fdr.DataReader('KS11', before, now)['Close']
-    datas = fdr.DataReader('KS11', '2021', '2022-12-31')['Close']
+    datas = fdr.DataReader('KS11', '2021')['Close']
     date = datas.index
 
     for i in date:
@@ -53,7 +67,7 @@ def get_exchangerate_data():
     now = datetime.now()
     before = now - relativedelta(years=10)
     # datas = fdr.DataReader('USD/KRW', before, now)['Close']
-    datas = fdr.DataReader('USD/KRW', '2012', '2022-12-31')['Close']
+    datas = fdr.DataReader('USD/KRW', '2022')['Close']
     datas = datas.replace({np.nan: None})
     date = datas.index
 
@@ -70,6 +84,8 @@ def get_exchangerate_data():
     print(f'FINISH update Exchange Rate table: running time {datetime.now() - now} secs & {len(datas)} datas')
 
 
-get_price_data()
-get_kospi_data()
-get_exchangerate_data()
+# get_price_data()
+# get_kospi_data()
+# get_exchangerate_data()
+
+asyncio.run(update_price_data_to_db())
