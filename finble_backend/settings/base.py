c7b34dfb537e5de134fefeb93d89654f48763096
geniee44 <68368633+geniee44@users.noreply.github.com>
2023-01-16 12:48:08 +0900

    change CRONJOBS

--- finble_backend/settings/base.py
+++ finble_backend/settings/base.py
@@ -141,5 +141,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRONJOBS = [
     ('30 16 * * MON-FRI', 'daily_update.update_kr_data_to_db_daily'),
-    ('0 7 * * TUE-SAT', 'daily_update.update_kr_data_to_db_daily'),
+    ('0 7 * * TUE-SAT', 'daily_update.update_us_data_to_db_daily'),
 ]
