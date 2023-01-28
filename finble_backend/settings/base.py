2d50d8c8dad4e42a22bcf9036d05b460963833b8
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2023-01-28 16:48:17 +0900

    update access token lifetime

--- finble_backend/settings/base.py
+++ finble_backend/settings/base.py
@@ -166,7 +166,7 @@ CRONJOBS = [
 ]
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'UPDATE_LAST_LOGIN': True,
 }
