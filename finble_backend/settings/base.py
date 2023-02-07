00d405a7ade9be6aa569ca0987894fa57c08d5cf
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2023-02-08 01:31:56 +0900

    cors origin whitelist (allow all = false)

--- finble_backend/settings/base.py
+++ finble_backend/settings/base.py
@@ -62,7 +62,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = (
+    "https://front.finble.net",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+)
+
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOW_METHODS = [
