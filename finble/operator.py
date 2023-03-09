65d9029f8e2614edd09db372533016175ff44dc3
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2023-03-09 17:18:34 +0900

    데이터 업데이트 시간 변경

--- finble/operator.py
+++ finble/operator.py
@@ -6,8 +6,8 @@ from finble.daily_update import test_print, update_kr_data_to_db_daily, update_u
 def start():
     scheduler = BackgroundScheduler(timezone='Asia/Seoul')
     # scheduler.add_job(test_print, CronTrigger.from_crontab('* * * * *'))
-    scheduler.add_job(update_kr_data_to_db_daily, CronTrigger.from_crontab('40 15 * * MON-FRI'))
+    scheduler.add_job(update_kr_data_to_db_daily, CronTrigger.from_crontab('30 16 * * MON-FRI'))
     scheduler.add_job(update_us_data_to_db_daily, CronTrigger.from_crontab('30 6 * * TUE-SAT'))
-    scheduler.add_job(update_kospi_data_to_db_daily, CronTrigger.from_crontab('40 15 * * MON-FRI'))
-    scheduler.add_job(update_exchangerate_data_to_db_daily, CronTrigger.from_crontab('40 15 * * MON-FRI'))
+    scheduler.add_job(update_kospi_data_to_db_daily, CronTrigger.from_crontab('30 16 * * MON-FRI'))
+    scheduler.add_job(update_exchangerate_data_to_db_daily, CronTrigger.from_crontab('30 16 * * MON-FRI'))
     scheduler.start()
